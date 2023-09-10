bb975fec39fbe5cfbfa103f02859e2c9080c6cc7
berylcl <gakiiberyl@gmail.com>
2023-09-10 19:03:23 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'category/index'
+  get 'category/new'
+  get 'category/create'
+  get 'expense/new'
+  get 'expense/index'
+  get 'expense/create'
+  get 'home/index'
+  get 'user/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
