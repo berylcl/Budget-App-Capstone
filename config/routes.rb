6c5310b7af25378ce9585143109dc57250d44f30
berylcl <gakiiberyl@gmail.com>
2023-09-11 06:22:19 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'category/index'
-  get 'category/new'
-  get 'category/create'
-  get 'expense/new'
-  get 'expense/index'
-  get 'expense/create'
-  get 'home/index'
-  get 'user/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :user, only: [:new] do
+    resources :category, only: [:index, :new, :create]
+  end
+  resources :expense, only: [:new, :create, :index]
+
+  authenticated(:user) do
+    root to: 'category#index', as: :authenticated_root
+  end
+  unauthenticated(:user) do
+    root 'home#index'
+  end
+
 end
