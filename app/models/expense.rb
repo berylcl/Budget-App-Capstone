2bd439b65fa21170d3a8a271c3d120cb5f451da2
Gakii Beryl <gakiiberyl@gmail.com>
2023-09-12 11:00:41 +0100

    Delete app directory

--- app/models/expense.rb
+++
@@ -1,8 +0,0 @@
-class Expense < ApplicationRecord
-  belongs_to :user
-  has_many :exp_cats
-  has_many :categories, through: :exp_cats
-
-  validates :name, :amount, presence: true
-  validates :amount, numericality: { greater_than: 0 }
-end
